345f856def32790354fd1356e93d9fd34e52f75c
Frank <frankzou2000@hotmail.com>
2019-10-14 22:34:22 +0000

    added 3_queries

---
+++ 3_queries_group-by/large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, count(students.id) AS student_count
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING count(students.id) >= 18
+ORDER BY count(students.id);
